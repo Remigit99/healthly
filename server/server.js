11b11235da4b954818438fc86e62796526763d48
Remigit99 <aderemibiodun1000@gmail.com>
2026-04-01 05:02:54 +0100

    modify start script for deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ import router from './routes/user.route.js';
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors(
+  origin: process.env.FRONTEND_URL || "*",
+  credentials: true
+))
 app.use(express.json())
 
 
